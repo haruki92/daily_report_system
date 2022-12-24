778727cb854282d12b7e140d79684d6cf3f6ebd4
haruki.kunihira <harukikunihira@gmail.com>
2022-12-24 15:56:50 +0900

    共通のメソッドfindOne(),countAll()をServiceBaseへ

--- src/main/java/services/EmployeeService.java
+++ src/main/java/services/EmployeeService.java
@@ -18,31 +18,6 @@ import utils.EncryptUtil;
 public class EmployeeService extends ServiceBase {
 
 	/**
-	 * 指定されたページ数の一覧画面に表示するデータを取得し、EmployeeViewのリストで返却する
-	 * @param page ページ数
-	 * @return 表示するデータのリスト
-	 */
-	public List<EmployeeView> getPerPage(int page) {
-		List<Employee> employees = em.createNamedQuery(JpaConst.Q_EMP_GET_ALL, Employee.class)
-				.setFirstResult(JpaConst.ROW_PER_PAGE * (page - 1))
-				.setMaxResults(JpaConst.ROW_PER_PAGE)
-				.getResultList();
-
-		return EmployeeConverter.toViewList(employees);
-	}
-
-	/**
-	 * 従業員テーブルのデータの件数を取得し、返却する
-	 * @return 従業員テーブルのデータの件数
-	 */
-	public long countAll() {
-		long empCount = em.createNamedQuery(JpaConst.Q_EMP_COUNT, Long.class)
-				.getSingleResult();
-
-		return empCount;
-	}
-
-	/**
 	 * 社員番号、パスワードを条件に取得したデータをEmployeeViewのインスタンスで返却する
 	 * @param code 社員番号
 	 * @param plainPass パスワード文字列
--- src/main/java/services/ServiceBase.java
+++ src/main/java/services/ServiceBase.java
@@ -1,7 +1,13 @@
 package services;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
+import actions.views.EmployeeConverter;
+import actions.views.EmployeeView;
+import constants.JpaConst;
+import models.Employee;
 import utils.DBUtil;
 
 // DB接続に関わる共通処理を行うクラス
@@ -14,5 +20,30 @@ public class ServiceBase {
 		if (em.isOpen()) {
 			em.close();
 		}
+	}
+
+	/**
+	 * 指定されたページ数の一覧画面に表示するデータを取得し、EmployeeViewのリストで返却する
+	 * @param page ページ数
+	 * @return 表示するデータのリスト
+	 */
+	public List<EmployeeView> getPerPage(int page) {
+		List<Employee> employees = em.createNamedQuery(JpaConst.Q_EMP_GET_ALL, Employee.class)
+				.setFirstResult(JpaConst.ROW_PER_PAGE * (page - 1))
+				.setMaxResults(JpaConst.ROW_PER_PAGE)
+				.getResultList();
+
+		return EmployeeConverter.toViewList(employees);
+	}
+
+	/**
+	 * 従業員テーブルのデータの件数を取得し、返却する
+	 * @return 従業員テーブルのデータの件数
+	 */
+	public long countAll() {
+		long empCount = em.createNamedQuery(JpaConst.Q_EMP_COUNT, Long.class)
+				.getSingleResult();
+
+		return empCount;
 	}
 }
